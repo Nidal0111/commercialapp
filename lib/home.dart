52db8169f60ba04fc4f9ecb789a2b234933daeb4
Nidal0111 <nidalpk47@gmial.com>
2025-06-28 13:02:04 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Homes extends StatelessWidget {
+  const Homes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      body: Column(
+      ),
+      appBar: AppBar(title: Text("MAX",style: TextStyle( ),),),
+    );
+  }
+}
